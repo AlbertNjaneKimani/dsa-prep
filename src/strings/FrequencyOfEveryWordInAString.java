4c3bb8619a2b79c8982ad5241fbe40e8eab561a8
AlbertNjaneKimani <albertnjanek@gmail.com>
2023-09-06 03:33:51 +0300

    updated code

--- src/strings/FrequencyOfEveryWordInAString.java
+++ src/strings/FrequencyOfEveryWordInAString.java
@@ -1,5 +1,5 @@
 package strings;
-
+import java.util.*;
 public class FrequencyOfEveryWordInAString {
     /*
     - Replace all punctuations with space
@@ -11,4 +11,38 @@ public class FrequencyOfEveryWordInAString {
         Time Complexity: O(n)
         Space Complexity: O(n)
     * */
+    public static Map<String, Integer> mostFrequent(String str) {
+
+        str = str.replaceAll("[^a-zA-Z0-9]", " ");
+        String[] allWords = str.split(" +");
+
+        Map<String, Integer> countingMap = new HashMap<>();
+
+        for (String word : allWords) {
+            word = word.toLowerCase();
+            countingMap.put(word, countingMap.getOrDefault(word, 0) + 1);
+        }
+
+        TreeMap<String, Integer> mostFrequentMap = new TreeMap<>((e1, e2) -> {
+            int freq1 = countingMap.get(e1);
+            int freq2 = countingMap.get(e2);
+
+            if (freq1 != freq2) {
+                return freq2 - freq1;
+            }
+
+            return e1.compareTo(e2);
+        });
+
+        mostFrequentMap.putAll(countingMap);
+
+        return mostFrequentMap;
+    }
+
+    public static void main(String[] args) {
+        String str = "Best items in category are Samsung lenovo. Samsung items are nice. Lenovo are cool";
+
+        System.out.println(FrequencyOfEveryWordInAString.mostFrequent(str));
+
+    }
 }
